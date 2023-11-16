e5f466910ec08babd6bb351b65b92ad29faa473d
An <pyop.van.an@gmail.com>
2023-11-16 02:22:13 -0500

    Step3

--- index.js
+++ index.js
@@ -29,23 +29,26 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
-
+    for (let i = 0; i < games.length; i++) {
         // create a new div element, which will become the game card
-
+        const gameCard = document.createElement('div');
 
         // add the class game-card to the list
-
+        gameCard.classList.add('game-card');
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
-
+        gameCard.innerHTML = `
+            <h1> The game ${games.name}</h1> 
+            <p> is ${games.description}</p>
+            <img ${game.img} class="game-img"> 
+        `;
         // append the game to the games-container
-
-}
+        document.body.appendChild(gameCard); 
+    }
+}   
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
